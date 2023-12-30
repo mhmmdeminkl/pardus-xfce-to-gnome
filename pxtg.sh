8a686bcdbb307953110f9f5aa69b89eaeed64513
Muhammed Emin Kola <152064891+mhmmdeminkl@users.noreply.github.com>
2023-12-30 20:58:44 +0300

    Update pxtg.sh
    Paket isimleri ayrı yazıldı, bazı düzenlemeler yapıldı.

--- pxtg.sh
+++ pxtg.sh
@@ -18,7 +18,7 @@ confirm() {
  
 # Verilen girdiye göre işlemin devamı veya iptali
 if confirm; then
-    echo "İşleme devam ediliyor lütfen bekleyiniz..."
+    echo "İşleme devam ediliyor. Lütfen root yetkisi için şifrenizi girebilir misiniz?"
 else
     echo "İşlem iptal edildi. Çıkış yapılıyor. Tekrar görüşmek üzere..."
     exit
@@ -27,17 +27,28 @@ fi
 # Güncelleme, XFCE yükleme işlemi ve GNOME arayüzüne dair tüm paketlerin silinmesi
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get -y install pardus-gnome-*
+sudo apt-get -y install pardus-gnome-desktop
+sudo apt-get -y install pardus-gnome-greeter
+sudo apt-get -y install pardus-gnome-settings
+sudo apt-get -y install pardus-gnome-shortcuts
 sudo dpkg-reconfigure gdm3
 sudo apt-get -y purge lightdm
-sudo apt-get -y purge pardus-xfce-*
+sudo apt-get -y purge pardus-xfce-desktop
+sudo apt-get -y purge pardus-xfce-greeter
+sudo apt-get -y purge pardus-xfce-gtk-theme
+sudo apt-get -y purge pardus-xfce-icon-theme
+sudo apt-get -y purge pardus-xfce-live-settings
+sudo apt-get -y purge pardus-xfce-settings
+sudo apt-get -y purge pardus-xfce-tweaks
+# Gnome arayüzünün Xfce arayüzü ile ilgili paketleri kullanma ihtimali bulunduğu için tekrar kullanıldı.
+sudo apt-get -y install pardus-gnome-*
 sudo apt-get -y autoremove
 sudo apt-get -y clean
 sudo apt-get -y autoclean
+# Pardus-Gnome-Greeter Pardus Karşılayıcı için autostart işlemi
+cp /usr/share/applications/tr.org.pardus.pardus-gnome-greeter.desktop ~/.config/autostart/
 
 echo "İşlem tamamlandı. Başka scriptlerde görüşmek üzere..."
-
-
 
 echo -e  "\033[31mDeğişikliklerin uygulanabilmesi için bilgisayarınız yeniden başlatılacaktır. Bu soruya onay vermelisiniz!\033[0m"
 
@@ -52,8 +63,7 @@ confirm() {
     done
 }
 
-# Pardus Karşılayıcı için autostart işlemi
-cp /usr/share/applications/tr.org.pardus.pardus-gnome-greeter.desktop ~/.config/autostart/
+
 # Verilen girdiye göre işlemin devamı
 if confirm; then
     echo " Bilgisayarınız yeniden başlatılıyor."
